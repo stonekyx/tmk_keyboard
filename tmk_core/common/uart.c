bd6ea040fd7991aaee498f0b0228da91039f0049
tmk <hasu@tmk-kbd.com>
2019-03-31 16:46:07 +0900

    core: Make uart_putchar() non-block and increase buffer size
    blocking function is not for debug purpose

--- tmk_core/common/uart.c
+++ tmk_core/common/uart.c
@@ -66,7 +66,7 @@
 
 // These buffers may be any size from 2 to 256 bytes.
 #define RX_BUFFER_SIZE 64
-#define TX_BUFFER_SIZE 40
+#define TX_BUFFER_SIZE 256
 
 static volatile uint8_t tx_buffer[TX_BUFFER_SIZE];
 static volatile uint8_t tx_buffer_head;
@@ -95,7 +95,7 @@ void uart_putchar(uint8_t c)
 
 	i = tx_buffer_head + 1;
 	if (i >= TX_BUFFER_SIZE) i = 0;
-	while (tx_buffer_tail == i) ; // wait until space in buffer
+	if (tx_buffer_tail == i) return;
 	//cli();
 	tx_buffer[i] = c;
 	tx_buffer_head = i;
